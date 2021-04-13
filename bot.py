035b85a3a5926782bdb3dd6b863295d72052c7b4
Ammar Amjawala <amjawala_am@India.XoriantCorp.com>
2021-04-13 15:13:00 +0530

    Redundant code removed

--- bot.py
+++ bot.py
@@ -69,78 +69,10 @@ def find_birthdays():
     return mailing_list
 
 
-def birthday_finder():
-    list_of_user_profile_links = []
-    next_page = True
-
-    while next_page:
-
-        for counter in range(1, 5):
-            '//*[@id="block-system-main"]/div/div/div/div[1]/div/div[5]/div/div/div/div/div/div/div[1]/div[1]/div[2]/span/a'
-            a_tag_xpath = '//*[@id="block-system-main"]/div/div/div/div[1]/div/div[5]/div/div/div/div/div/div/div[1]/div[' + str(
-                counter) + ']/div[2]/span/a'
-            # '//*[@id="block-system-main"]/div/div/div/div[1]/div/div[5]/div/div/div/div/div/div/div[1]/div[1]/div[2]/span/a'
-            check_a_selection = chrome.find_element_by_xpath(a_tag_xpath)
-            user_profile_link = check_a_selection.get_attribute('href')
-
-            dob_div_xpath = '//*[@id="block-system-main"]/div/div/div/div[1]/div/div[5]/div/div/div/div/div/div/div[1]/div[' + str(
-                counter) + ']/div[3]/div'
-            dob = chrome.find_element_by_xpath(dob_div_xpath).text
-
-            if (int(dob[:2]) == int(date.today().day)):
-                list_of_user_profile_links.append(user_profile_link)
-            else:
-                next_page = False
-                break
-
-            if counter == 4:
-                current_page_li = chrome.find_element_by_xpath(
-                    '//*[@id="block-system-main"]/div/div/div/div[1]/div/div[5]/div/div/div/div/div/div/div[2]/ul/li[2]')
-                current_page_number = (current_page_li.text[0])
-                chrome.get('https://xornet.xoriant.com/front?page=' + current_page_number)
-
-    return list_of_user_profile_links
-
-
-def get_email(list_of_user_profile_links):
-    mailing_list = []
-
-    for user_url in list_of_user_profile_links:
-        chrome.get(user_url)
-        mail_address_div = chrome.find_element_by_xpath(
-            '//*[@id="block-system-main"]/div/div/div[3]/div/div[2]/div/div[2]/div/div/div/div/div/p/a')
-        mailing_list.append(mail_address_div.text)
-
-    return mailing_list
-
-
 def fire_wishes(mailing_list):
     outlook = wc.Dispatch('Outlook.Application')
     flag = 0
     for i in mailing_list:
-        '''if i != 'Renu.Deshpande@Xoriant.Com' and flag == 0:
-            continue
-        flag = 1
-        i = i.split()[0] +" "+ i.split()[len(i.split())-1]
-        print (i)
-        recipient = outlook.Session.CreateRecipient(i)
-        recipient.Resolve()
-        print('Resolved OK: ', recipient.Resolved)
-        print('Is it a sendable? (address): ', recipient.Sendable)
-        print('Name: ', recipient.Name)
-
-        ae = recipient.AddressEntry
-        email_address = None
-
-        if 'EX' == ae.Type:
-            eu = ae.GetExchangeUser()
-            email_address = eu.PrimarySmtpAddress
-
-        if 'SMTP' == ae.Type:
-            email_address = ae.Address
-
-        print('Email address: ', email_address)
-        '''
         msg = outlook.CreateItem(0)
         msg.To = i
         msg.Subject = 'Happy Birthday!'
@@ -157,7 +89,5 @@ def fire_wishes(mailing_list):
 if __name__ == '__main__':
     login_xornet()
     mailing_list = find_birthdays()
-    # list_of_user_profile_links = birthday_finder()
-    # mailing_list = get_email(list_of_user_profile_links)
     fire_wishes(mailing_list)
     chrome.close()
